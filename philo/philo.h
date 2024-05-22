fd47d3fb1407de321981e342be198ab4ee6d9ce1
Younes Ounajjar <younajja@c3r9p3.1337.ma>
2024-05-22 20:01:08 +0100

    up

--- philo/philo.h
+++ philo/philo.h
@@ -7,26 +7,43 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+typedef	struct s_fork t_fork;
+typedef struct s_philo t_philo;
 
 typedef struct t_data{
-    int     nb_philo;
-    int     time_to_die;
-    int     time_to_eat;
-    int     time_to_sleep;
-    int     eating_nb;
+	//args form inputs
+    int     nb_philo; //av[1] ✅
+    int     time_to_die; //av[2] ✅
+    int     time_to_eat; //av[3] ✅
+    int     time_to_sleep; //av[4] ✅
+    int     limit_meals; //  opt av[5] ✅
+
+	// data
+	//int eat;
+	//size_t time;
+	t_fork *forks; // ✅
+	t_philo *philosophers; //✅
+
 } t_data;
 
 
-typedef struct s_philo
+struct s_fork{
+	int fork_id; //✅
+	pthread_mutex_t fork; //✅
+};
+
+
+struct s_philo
 {
-	int				philo_id;
+	int				philo_id; //✅
 	pthread_t		thread_id;
-	//size_t			should_die;
-	//int				eat_max;
-	t_data          *data;
-	//pthread_mutex_t	fork;
-	//pthread_mutex_t	*next_fork;
-}	t_philo;
+	size_t			time_last_meal;
+	int				count_meals; //✅
+	int				full; //✅
+	t_data          *data; //✅
+	t_fork			*first_fork; //✅
+	t_fork			*second_fork; //✅
+};
 
 
 
--- philo/setting.c
+++ philo/setting.c
@@ -1,21 +1,41 @@
 #include "philo.h"
 
+int check_digit(char **strs)
+{
+    int i = 1;
+    while (strs[i])
+    {
+        int j = 0;
+        while (strs[i][j])
+        {
+            if (!(strs[i][j] >= '0' && strs[i][j] <= '9'))
+                return (1);
+            j++;
+        }
+        i++;
+    }
+    return (0);
+}
+
+int check_args(int ac, char **av)
+{
+    if ((ac != 5 && ac != 6) || check_digit(av) == 1)
+        return (1);
+    return (0);
+}
 
 int ft_atoi(char *str)
 {
     int i = 0;
     int sign = 1;
     int res = 0;
-
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
+    int prev = 0;
     while (str[i] >= '0' && str[i] <= '9')
     {
+        prev = res;
         res = res * 10 + str[i] - 48;
+        if (prev != res / 10)
+            return (0);
         i++;
     }
     return (res * sign);
@@ -28,11 +48,9 @@ int setting_up(t_data *data, char **av, int ac)
     data->time_to_eat = ft_atoi(av[3]);
     data->time_to_sleep = ft_atoi(av[4]);
     if (ac == 6)
-        data->eating_nb = ft_atoi(av[5]);
+        data->limit_meals = ft_atoi(av[5]);
     if (data->nb_philo < 1 || data->time_to_die < 1 || data->time_to_eat < 1
-        ||  data->time_to_sleep < 1 || data->eating_nb < 1)
+        ||  data->time_to_sleep < 1)
         return (1);
-    if (ac == 5)
-        data->eating_nb = 0;
     return (0);
 }
